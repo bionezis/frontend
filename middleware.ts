1acdac6f606d1d8c41bd2e2274ab855b7fb50ffc
Lukas Arbaczewski <l.arbaczewski@gmail.com>
2025-10-25 08:21:35 +0200

    Update middleware configuration to follow Next.js 15+ conventions

--- middleware.ts
+++ middleware.ts
@@ -54,7 +54,12 @@ export const config = {
   // Match all pathnames except for
   // - … if they start with `/api`, `/_next` or `/_vercel`
   // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
+  matcher: [
+    // Match all pathnames except for
+    // - … if they start with `/api`, `/_next` or `/_vercel`
+    // - … the ones containing a dot (e.g. `favicon.ico`)
+    '/((?!api|_next|_vercel|.*\\..*).*)',
+  ],
 };
 
 
